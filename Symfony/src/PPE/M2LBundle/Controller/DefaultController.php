dd79f2bc9b5cc1e2654ebf3d1fce9a2d3438f8fa
bmagne <benjamin.magne@y-nov.com>
2014-11-19 17:21:14 +0100

    Ajout de la page actualite

--- Symfony/src/PPE/M2LBundle/Controller/DefaultController.php
+++ Symfony/src/PPE/M2LBundle/Controller/DefaultController.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace PPE\M2LBundle\Controller;
-
+use PPE\M2LBundle\Entity\actualite;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PPEM2LBundle:Default:index.html.twig', array());
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+        $repo = $em->getRepository("PPEM2LBundle:actualite");
+        $actualiteList = $repo->findAll();
+        return $this->render('PPEM2LBundle:Default:index.html.twig', array("actualiteList"=>$actualiteList));
     }
+
 }
