5997bec05efad90a077f8019ef40d9a82454247a
bmagne <benjamin.magne@y-nov.com>
2014-11-23 02:14:24 +0100

    page info done

--- Symfony/src/PPE/M2LBundle/Resources/public/js/information.js
+++ Symfony/src/PPE/M2LBundle/Resources/public/js/information.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
 	$('.information_content').hide();
 	$('.information_titre').click(function(){
-		$(this).children('.information_content').toggle(400);
+		$('.expanded').removeClass('expanded').addClass('closed').next('.information_content').not(':animated').slideUp();
+		$(this).removeClass('closed').addClass('expanded').next('.information_content').not(':animated').slideDown();			
 	});
+	$('.information_titre').hover(function(){
+		$(this).css("background", "#18B293");
+	},function(){
+		$(this).css("background", "#1abc9c");
+    }
+	)
 });
