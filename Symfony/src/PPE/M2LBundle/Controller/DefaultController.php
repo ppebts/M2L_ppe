5aff50683ca5551dd85b7b2f95d11bc73909c98e
nsken <maurayflorian@gmail.com>
2015-01-14 11:57:49 +0100

    Commit
    Commit

--- Symfony/src/PPE/M2LBundle/Controller/DefaultController.php
+++ Symfony/src/PPE/M2LBundle/Controller/DefaultController.php
@@ -49,14 +49,14 @@ public function contactAction()
     {
         return $this->render('PPEM2LBundle:Default:contact.html.twig', array());
     } 
-public function connexionAction()
+/* public function connexionAction()
     {
         return $this->render('PPEM2LBundle:Default:connexion.html.twig', array());
     }  
 public function inscriptionAction()
     {
         return $this->render('PPEM2LBundle:Default:inscription.html.twig', array());
-    }  
+    }  */
 public function accountAction()
     {
         return $this->render('PPEM2LBundle:Default:account.html.twig', array());
