050c9d1aa39ee444b7bb4bbdd4dbdd81c973c0da
nsken <maurayflorian@gmail.com>
2015-01-19 19:49:20 +0100

    Back-office
    Formations

--- Symfony/src/PPE/M2LBundle/Controller/AdminController.php
+++ Symfony/src/PPE/M2LBundle/Controller/AdminController.php
@@ -7,6 +7,9 @@ use PPE\M2LBundle\Form\Type\InformationsType;
 use PPE\M2LBundle\Entity\Annonce;
 use PPE\M2LBundle\Form\Type\AnnonceType;
 use PPE\M2LBundle\Form\Type\AnnoncesType;
+use PPE\M2LBundle\Entity\Formation;
+use PPE\M2LBundle\Form\Type\FormationType;
+use PPE\M2LBundle\Form\Type\FormationsType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,8 +18,8 @@ use Symfony\Component\HttpFoundation\Request;
 class AdminController extends Controller
 {
 
-
-public function getinformationsAction()
+                                                    /* Informations */
+    public function getinformationsAction()
      {
         /* Récupère toutes les informations */
 
@@ -28,7 +31,8 @@ public function getinformationsAction()
 
          return $this->render('PPEM2LBundle:Information:listeinfo.html.twig', array("infoList"=>$infoList));
      }
-public function addinformationAction(Request $request)
+    
+    public function addinformationAction(Request $request)
     {
         $information = new Information();
         
@@ -43,7 +47,7 @@ public function addinformationAction(Request $request)
         return $this->render('PPEM2LBundle:Information:information.html.twig', array("form"=>$form->createView(),));
     }
 
-public function editinformationAction($id,Request $request)
+    public function editinformationAction($id,Request $request)
      {
         /* Récupère l'information */
 
@@ -70,9 +74,38 @@ public function editinformationAction($id,Request $request)
          return $this->render('PPEM2LBundle:Information:editinfo.html.twig', array('form'=>$form->createView()));
      
     }
+                                                /* Fin Infomations */
+                                                /* Annonces */
+
+    public function getannoncesAction()
+     {
+        /* Récupère toutes les annonces */
+
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+        $repo = $em->getRepository("PPEM2LBundle:Annonce");
+        $annonceList = $repo->findAll();
 
 
-public function editannonceAction($id,Request $request)
+         return $this->render('PPEM2LBundle:Annonce:listeannonce.html.twig', array("annonceList"=>$annonceList));
+     }
+
+    public function addannonceAction(Request $request)
+    {
+        $annonce = new Annonce();
+        
+        $form = $this->get('form.factory')->create(new AnnonceType(),$annonce);
+        $form->handleRequest($request);
+        if ( $form->isValid()){
+            $doctrine = $this->getDoctrine();
+            $em = $doctrine->getManager();
+            $em->persist($annonce);
+            $em->flush();
+        }
+        return $this->render('PPEM2LBundle:Annonce:addannonce.html.twig', array("form"=>$form->createView(),));
+    }
+
+    public function editannonceAction($id,Request $request)
      {
         /* Récupère l'annonce */
 
@@ -99,33 +132,62 @@ public function editannonceAction($id,Request $request)
          return $this->render('PPEM2LBundle:Annonce:editannonce.html.twig', array('form'=>$form->createView()));
      
     }
-public function getannoncesAction()
+                                                /* Fin Annonces */
+                                                /* Formations */
+
+    public function getformationsAction()
      {
-        /* Récupère toutes les annonces */
+        /* Récupère toutes les formations */
 
         $doctrine = $this->getDoctrine();
         $em = $doctrine->getManager();
-        $repo = $em->getRepository("PPEM2LBundle:Annonce");
-        $annonceList = $repo->findAll();
+        $repo = $em->getRepository("PPEM2LBundle:Formation");
+        $formationList = $repo->findAll();
 
 
-         return $this->render('PPEM2LBundle:Annonce:listeannonce.html.twig', array("annonceList"=>$annonceList));
+         return $this->render('PPEM2LBundle:Formation:listeformation.html.twig', array("formationList"=>$formationList));
      }
 
-public function addannonceAction(Request $request)
+    public function addformationAction(Request $request)
     {
-        $annonce = new Annonce();
+        $formation = new Formation();
         
-        $form = $this->get('form.factory')->create(new AnnonceType(),$annonce);
+        $form = $this->get('form.factory')->create(new FormationType(),$formation);
         $form->handleRequest($request);
         if ( $form->isValid()){
             $doctrine = $this->getDoctrine();
             $em = $doctrine->getManager();
-            $em->persist($annonce);
+            $em->persist($formation);
             $em->flush();
         }
-        return $this->render('PPEM2LBundle:Annonce:addannonce.html.twig', array("form"=>$form->createView(),));
+        return $this->render('PPEM2LBundle:Formation:addformation.html.twig', array("form"=>$form->createView(),));
     }
+        public function editformationAction($id,Request $request)
+     {
+        /* Récupère la formation */
 
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+        $repo = $em->getRepository("PPEM2LBundle:Formation");
+        $formation = $repo->find($id);
 
+        /* $formation renseigné donc formulaire renseigné */
+
+        $form = $this->get('form.factory')->create(new FormationsType(),$formation);
+        $form->handleRequest($request);  
+
+        /* Vérification du formulaire */
+
+        if ( $form->isValid()){
+                $doctrine = $this->getDoctrine();
+                $em = $doctrine->getManager();
+                $em->persist($formation);
+                $em->flush();
+                return $this->redirect($this->generateUrl('ppe_m2l_back_formationedit', array('id'=> $formation->getId())));
+
+        }        
+         return $this->render('PPEM2LBundle:Formation:editformation.html.twig', array('form'=>$form->createView()));
+     
+    }    
+                                                /* Fin Formations */
 }
---
+++ Symfony/src/PPE/M2LBundle/Form/Type/FormationsType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PPE\M2LBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FormationsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', 'text')
+            ->add('description', 'textarea')
+            ->add('duree', 'text')
+            ->add('debut', 'date')
+            ->add('lieu', 'text')
+            ->add('image', 'text')
+            ->add('nbparticipants', 'text')
+            ->add('publier', 'submit')
+            ->getForm();
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PPE\M2LBundle\Entity\Formation'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ppe_m2l_back_formations';
+    }
+}
