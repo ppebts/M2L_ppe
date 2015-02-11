2dcc6875b6864b78ac628163c87087315f955ff1
nsken <maurayflorian@gmail.com>
2015-02-11 09:26:12 +0100

    Test
    Commit

--- Symfony/src/PPE/M2LBundle/Controller/ContactController.php
+++ Symfony/src/PPE/M2LBundle/Controller/ContactController.php
@@ -3,8 +3,6 @@
 namespace PPE\M2LBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use PPE\M2LBundle\Entity\Contact;
-//use PPE\M2LBundle\Form\Type\ContactType;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -18,40 +16,23 @@ public function contactAction(Request $request)
         ->add('name', 'text')
         ->add('email', 'email')
         ->add('message', 'textarea')
+        ->add('sujet', 'text')
         ->add('envoyer', 'submit', array('attr' => array('class' => 'ppe_submit')))
         ->getForm();
 
     $form->handleRequest($request);
 
     if ($form->isValid()) {
-        // Les données sont un tableau avec les clés "name", "email", et "message"
         $data = $form->getData();
-        
-        //mettre php envoie email
-
-        // To
-        $to = 'joffrey.dota@gmail.com';
-         
-        // Subject
-        $subject = 'M2L.com - Mail Contact';
-         
-        // Headers
-        $headers = 'Mime-Version: 1.0'."\r\n";
-        $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
-        $headers .= "\r\n";
-         
-        // Message
-        $msg = '<strong>Developpez.com</strong> - Message du mail ...';
-         
-        // Function mail()
-        mail($to, $subject, $msg, $headers);
+    $message = \Swift_Message::newInstance()
+        ->setSubject($data["sujet"])
+        ->setFrom('m2lorraine.ppe@gmail.com')
+        ->setTo($data["email"])
+        ->setBody($data["message"])
+    ;
+    $this->get('mailer')->send($message);
 
             }
-
-            // ... affiche le formulaire
-
-
-        return $this->render('PPEM2LBundle:Contact:contact.html.twig', array("form"=>$form->createView(),
-            ));
+return $this->render('PPEM2LBundle:Contact:contact.html.twig', array('form'=>$form->createView()));
     }
 }
